10ee6712a826fb696489ec85fd0b709d14d454f5
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2025-05-27 06:28:22 +0300

    fix deploy error

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,17 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({ 
+        origin: [
+            "http://localhost:5173" , 
+            "https://am-stock-app.vercel.app",
+        ], 
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(morgan('dev'));
 
